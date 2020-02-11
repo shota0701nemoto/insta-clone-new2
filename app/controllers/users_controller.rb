bfafecf958a94c3b4c196de5b2686318ec38cb32
nemotoshogo <shota0701nemoto@gmail.com>
2020-02-11 14:51:57 +0900

    ユーザーの更新、削除、表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,19 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy] #index、edit、update、destroyアクションにはログインしないといけない。
+  before_action :correct_user,   only: [:edit, :update] #ログインしているユーザーのみ編集、更新ができる
+  before_action :admin_user,     only: :destroy #管理者だけがユーザー削除できる
 
   # GET /users/:id
+
+  def index
+    @users = User.paginate(page: params[:page]) #Userテーブルのデータを全て表示
+end
+
+
   def show
-    @user = User.find(params[:id])
+    @user = User.find(params[:id])  #findはモデルの検索機能を持つメソッド。モデルと紐づいているデータベースのテーブルからレコードを1つ取り出す際に使う。
+                                    #paramsはRails（route?）で送られてきた値を受け取るためのメソッド。get、post、formを使って送信される。
+                                    #params[:カラム名]
     # => app/views/users/show.html.erb
     # debugger
   end
@@ -11,6 +22,12 @@ class UsersController < ApplicationController
     @user = User.new
     # => form_for @user
   end
+
+  def edit
+    @user = User.find(params[:id]) #UsersControllerのインスタンス変数@user（@から始めるのは決まり事）を作成してeditメソッドを呼び出す。
+                                   #インスタンス変数を経由することでControllerからViewへ変数を渡すことができる。
+  end
+
 
   # POST /users
   def create
@@ -27,9 +44,53 @@ class UsersController < ApplicationController
     end
   end
 
+ def update
+   @user = User.find(params[:id])
+   if @user.update_attributes(user_params) #update_attributesはデータベースのレコードを複数同時に更新する。validationも実行される。
+     flash[:success] = "Profile updated"
+     redirect_to @user #
+  else
+    render'edit'
+   end
+  end
+
+ def destroy
+   User.find(params[:id]).destroy #findメソッドとdestroyメソッドを1行で書くために連結している
+   flash[:success] = "User deleted"
+   redirect_to users_url
+ end
+
+  private
+
+
   def user_params
     params.require(:user).permit(
       :name, :email, :password,
       :password_confirmation)
   end
-end
+
+  # beforeアクション
+
+  # ログイン済みユーザーかどうか確認
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+    end
+
+  # 正しいユーザーかどうか確認
+  def correct_user
+    @user = User.find(params[:id])
+     redirect_to(root_url) unless current_user?(@user)
+  end
+
+  # 管理者かどうか確認
+  def admin_user
+   redirect_to(root_url) unless current_user.admin?
+  end
+
+
+
+    end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2017_08_13_084029) do
+ActiveRecord::Schema.define(version: 2020_02_11_052257) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2017_08_13_084029) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
