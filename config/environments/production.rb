4535772f7ff8a32c57e804decf2e2e59d4f22f2c
nemotoshogo <shota0701nemoto@gmail.com>
2020-02-13 11:48:28 +0900

    Sendgridのusername、passwprdを再登録

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
 
 
 
-  config.action_mailer.raise_delivery_errors = true
+config.action_mailer.raise_delivery_errors = true
 config.action_mailer.delivery_method = :smtp
 host = 'https://insta-clone-new2.herokuapp.com'
 config.action_mailer.default_url_options = { host: host }
